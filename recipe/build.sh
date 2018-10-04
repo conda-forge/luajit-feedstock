b2f075de487b02e99180e07339e764ad93958f12
Marcelo Duarte Trevisani <marcelotrevisani@users.noreply.github.com>
2018-10-04 10:54:09 +0200

    Removed target_sys

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-make PREFIX="$PREFIX" CC="${CC}" TARGET_SYS="$(uname)" MULTILIB="${PREFIX}/lib"
+make PREFIX="$PREFIX" CC="${CC}" MULTILIB="${PREFIX}/lib"
 make install PREFIX="$PREFIX"
