7d8b73d691c3c2a77222dcb530189673a8d8d7db
Bas Zalmstra <4995967+baszalmstra@users.noreply.github.com>
2025-05-27 13:14:28 +0200

    setup correct include dirs

--- recipe/build.bat
+++ recipe/build.bat
@@ -5,9 +5,9 @@ copy lua51.dll %LIBRARY_BIN%
 mkdir -p %LIBRARY_LIB%
 copy lua51.lib %LIBRARY_LIB%
 copy luajit.lib %LIBRARY_LIB%
-mkdir %LIBRARY_INC%\luajit-2.1
-xcopy *.h %LIBRARY_INC%\luajit-2.1
-xcopy jit %LIBRARY_INC%\luajit-2.1 /s /e
+mkdir %LIBRARY_INC%\luajit-2.0
+xcopy *.h %LIBRARY_INC%\luajit-2.0
+xcopy jit %LIBRARY_INC%\luajit-2.0 /s /e
 
 goto :EOF
 
